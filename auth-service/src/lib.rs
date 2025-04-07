ef40a6dc254a6cdf880617aec011cba152965852
RockNimbus <ec@rockecom.net>
2025-04-07 19:58:44 +0100

    sprint 1 task 4 Structure the auth service for testing

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,40 @@
+// This struct encapsulates our application-related logic.
+use axum::{response::Html, routing::get, serve::Serve, Router};
+use tower_http::services::ServeDir;
+use std::error::Error;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Move the Router definition from `main.rs` to here.
+        // Also, remove the `hello` route.
+        // We don't need it at this point!
+        let router = Router::new()
+        .nest_service("/", ServeDir::new("assets"));
+        // .route("/hello", get(hello_handler)); // Leaving for example
+
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        let app = Application{server, address};
+        Ok(app)
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+// Leaving as an example 
+// async fn hello_handler() -> Html<&'static str> {
+//     Html("<h1>Hello, Rock Automaton!</h1>")
+// }
