63b31945e518584577f4db95ea4fe879677dba15
RockNimbus <ec@rockecom.net>
2025-04-08 16:42:15 +0100

    Sprint 1 - Task 5 - New routes and tests for them

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,7 +1,14 @@
 // This struct encapsulates our application-related logic.
-use axum::{response::Html, routing::get, serve::Serve, Router};
-use tower_http::services::ServeDir;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    routing::post,
+    serve::Serve,
+    Router,
+};
 use std::error::Error;
+use tower_http::services::ServeDir;
 
 pub struct Application {
     server: Serve<Router, Router>,
@@ -10,22 +17,40 @@ pub struct Application {
     pub address: String,
 }
 
+// Takes email, password, and 2fa code as input
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response() 
+}    
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
-        // Move the Router definition from `main.rs` to here.
-        // Also, remove the `hello` route.
-        // We don't need it at this point!
         let router = Router::new()
-        .nest_service("/", ServeDir::new("assets"));
-        // .route("/hello", get(hello_handler)); // Leaving for example
-
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
         let server = axum::serve(listener, router);
 
         // Create a new Application instance and return it
-        let app = Application{server, address};
+        let app = Application { server, address };
         Ok(app)
     }
 
@@ -34,7 +59,5 @@ impl Application {
         self.server.await
     }
 }
-// Leaving as an example 
-// async fn hello_handler() -> Html<&'static str> {
-//     Html("<h1>Hello, Rock Automaton!</h1>")
-// }
+
+// Leaving as an example
