30f066f9305514d0df16393edb69ac5113aed558
RockNimbus <ec@rockecom.net>
2025-04-09 15:05:19 +0100

    sprint 1 - task 6 - routes into module

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -10,6 +10,8 @@ use axum::{
 use std::error::Error;
 use tower_http::services::ServeDir;
 
+pub mod routes;
+
 pub struct Application {
     server: Serve<Router, Router>,
     // address is exposed as a public field
@@ -18,32 +20,20 @@ pub struct Application {
 }
 
 // Takes email, password, and 2fa code as input
-async fn signup() -> impl IntoResponse {
-    StatusCode::OK.into_response() 
-}    
 
-async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn verify_2fa() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
+
+
+
 
 impl Application {
     pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
         let router = Router::new()
             .nest_service("/", ServeDir::new("assets"))
-            .route("/signup", post(signup))
-            .route("/login", post(login))
-            .route("/logout", post(logout))
-            .route("/verify-2fa", post(verify_2fa))
-            .route("/verify-token", post(verify_token));
+            .route("/signup", post(routes::signup))
+            .route("/login", post(routes::login))
+            .route("/logout", post(routes::logout))
+            .route("/verify-2fa", post(routes::verify_2fa))
+            .route("/verify-token", post(routes::verify_token));
 
         let listener = tokio::net::TcpListener::bind(address).await?;
         let address = listener.local_addr()?.to_string();
@@ -60,4 +50,3 @@ impl Application {
     }
 }
 
-// Leaving as an example
