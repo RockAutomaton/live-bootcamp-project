7bc2017e6669f888fa2ff84114740199775f1613
RockNimbus <ec@rockecom.net>
2025-04-23 23:21:42 +0100

    Updated verify token  to work better.

--- auth-service/src/routes/verify_token.rs
+++ auth-service/src/routes/verify_token.rs
@@ -2,28 +2,17 @@ use axum::{http::StatusCode, response::IntoResponse, Json};
 use axum_extra::extract::CookieJar;
 use serde::Deserialize;
 
-use crate::domain::{AuthAPIError, Token};
-use crate::utils::{auth::validate_token, constants::JWT_COOKIE_NAME};
+use crate::domain::AuthAPIError;
+use crate::utils::auth::validate_token;
 
 pub async fn verify_token(
-    jar: CookieJar,
+    _jar: CookieJar,
     Json(request): Json<TokenRequest>,
 ) -> Result<impl IntoResponse, AuthAPIError> {
-    // First check if we have an auth cookie
-    let auth_cookie = jar.get(JWT_COOKIE_NAME)
-        .ok_or(AuthAPIError::InvalidToken)?;
-
-    // Validate the token from the cookie
-    match validate_token(auth_cookie.value()).await {
-        Ok(_) => {},
-        Err(_) => return Err(AuthAPIError::InvalidToken),
-    }
-
-    // Then validate the token from the request
-    let _ = match Token::parse(&request.token) {
-        Ok(_) => {}
-        Err(_) => return Err(AuthAPIError::InvalidToken),
-    };
+    // Validate the JWT token
+    validate_token(&request.token)
+        .await
+        .map_err(|_| AuthAPIError::InvalidToken)?;
 
     Ok(StatusCode::OK.into_response())
 }
